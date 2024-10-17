d77db31235d5332aa99f61eca23813a6b026f96d
Charlie <charlie_xumeng@hotmail.com>
2024-10-17 14:54:51 +1030

    save

---
+++ module03/ex02/ClapTrap.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+protected:
+	std::string _name;
+	int _hp;
+	int _energy;
+	unsigned int _dmg;
+
+public:
+	ClapTrap(std::string str);
+	~ClapTrap();
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	void info();
+};
---
+++ module03/ex02/FragTrap.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+private:
+	/* data */
+public:
+	FragTrap(std::string str);
+	~FragTrap();
+	void highFiveGuys(void);
+};

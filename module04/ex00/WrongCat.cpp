8ff8e34025cce40d21b43adb9e05a60dcfbf65d7
Charlie <charlie_xumeng@hotmail.com>
2024-10-18 14:05:24 +1030

    save

---
+++ module04/ex00/WrongCat.cpp
@@ -0,0 +1,32 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat(/* args */)
+{
+	type = "WrongCat";
+	std::cout << "WrongCat constructor called." << std::endl;
+}
+
+WrongCat::~WrongCat()
+{
+	std::cout << "WrongCat destructor called." << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat &other) : WrongAnimal(other)
+{
+	std::cout << "WrongCat copy constructor called."
+			  << "Type is: " << type << std::endl;
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &other)
+{
+	if (this == &other)
+		return *this;
+	WrongAnimal::operator=(other);
+	return *this;
+	std::cout << "WrongCat " << type << " is assigned." << std::endl;
+}
+
+void WrongCat::makeSound(void) const
+{
+	std::cout << "WrongCat " << type << " says: I want coffee!" << std::endl;
+}
--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -1,19 +1,25 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
 	const Animal *meta = new Animal();
 	const Animal *j = new Dog();
 	const Animal *i = new Cat();
+	const WrongAnimal *k = new WrongCat();
+
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
+	k->makeSound();
 	delete meta;
 	delete i;
 	delete j;
+	delete k;
 	return 0;
 }

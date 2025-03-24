adf5799a30def6241ed0ab72128062029875cfae
charlie <charlie@ubuntu>
2025-03-24 06:49:29 +0000

    save

--- module06/ex00/include/ScalarConverter.hpp
+++ module06/ex00/include/ScalarConverter.hpp
@@ -1,17 +1,37 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <exception>
+#include <limits>
+#include <cctype>
+#include <cmath>
+#include <cstdlib>
+
+enum scalarType
+{
+	NAN_,
+	INF,
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	INVALID
+};
 
 class ScalarConverter
 {
 private:
-	std::string _input;
+	
+	const std::string _input;
 	
 	ScalarConverter();
 	~ScalarConverter();
 	ScalarConverter(std::string const &input);
 	ScalarConverter(const ScalarConverter &other);
 	ScalarConverter &operator=(const ScalarConverter &other);
-public:
+
+	
+	public:
 	static void convert(std::string const &input);
+	
 };
---
+++ module06/ex00/src/main.cpp
@@ -0,0 +1,13 @@
+#include "ScalarConverter.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./convert [value]" << std::endl;
+		return 1;
+	}
+	ScalarConverter::convert(argv[1]);
+	return 0;
+	
+}
